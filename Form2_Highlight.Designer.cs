91e9ba263c00bb7103feb083043fc99cae598707
erik.juvan <erik.juvan@gorenje.com>
2022-04-02 00:04:47 +0200

    bunch of highlight work

--- Form2_Highlight.Designer.cs
+++ Form2_Highlight.Designer.cs
@@ -32,6 +32,7 @@
             this.button_ok = new System.Windows.Forms.Button();
             this.button_cancel = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
             this.button_add = new System.Windows.Forms.Button();
             this.button_delete = new System.Windows.Forms.Button();
             this.button_moveup = new System.Windows.Forms.Button();
@@ -50,52 +51,73 @@
             // 
             // button_ok
             // 
-            this.button_ok.Location = new System.Drawing.Point(182, 414);
+            this.button_ok.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_ok.Location = new System.Drawing.Point(182, 426);
             this.button_ok.Name = "button_ok";
             this.button_ok.Size = new System.Drawing.Size(81, 23);
             this.button_ok.TabIndex = 0;
             this.button_ok.Text = "OK";
             this.button_ok.UseVisualStyleBackColor = true;
+            this.button_ok.Click += new System.EventHandler(this.button_ok_Click);
             // 
             // button_cancel
             // 
-            this.button_cancel.Location = new System.Drawing.Point(269, 414);
+            this.button_cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_cancel.Location = new System.Drawing.Point(269, 426);
             this.button_cancel.Name = "button_cancel";
             this.button_cancel.Size = new System.Drawing.Size(81, 23);
             this.button_cancel.TabIndex = 1;
             this.button_cancel.Text = "Cancel";
             this.button_cancel.UseVisualStyleBackColor = true;
-            this.button_cancel.Click += new System.EventHandler(this.button2_Click);
+            this.button_cancel.Click += new System.EventHandler(this.button_cancel_Click);
             // 
             // listView1
             // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.listView1.GridLines = true;
             this.listView1.Location = new System.Drawing.Point(12, 12);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(338, 210);
+            this.listView1.ShowGroups = false;
+            this.listView1.Size = new System.Drawing.Size(338, 222);
             this.listView1.TabIndex = 2;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "";
+            this.columnHeader1.Width = 300;
             // 
             // button_add
             // 
-            this.button_add.Location = new System.Drawing.Point(12, 228);
+            this.button_add.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_add.Location = new System.Drawing.Point(12, 240);
             this.button_add.Name = "button_add";
             this.button_add.Size = new System.Drawing.Size(81, 23);
             this.button_add.TabIndex = 3;
             this.button_add.Text = "Add";
             this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.button_add_Click);
             // 
             // button_delete
             // 
-            this.button_delete.Location = new System.Drawing.Point(97, 228);
+            this.button_delete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_delete.Location = new System.Drawing.Point(97, 240);
             this.button_delete.Name = "button_delete";
             this.button_delete.Size = new System.Drawing.Size(81, 23);
             this.button_delete.TabIndex = 4;
             this.button_delete.Text = "Delete";
             this.button_delete.UseVisualStyleBackColor = true;
+            this.button_delete.Click += new System.EventHandler(this.button_delete_Click);
             // 
             // button_moveup
             // 
-            this.button_moveup.Location = new System.Drawing.Point(182, 228);
+            this.button_moveup.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_moveup.Location = new System.Drawing.Point(182, 240);
             this.button_moveup.Name = "button_moveup";
             this.button_moveup.Size = new System.Drawing.Size(81, 23);
             this.button_moveup.TabIndex = 5;
@@ -104,7 +126,8 @@
             // 
             // button_movedown
             // 
-            this.button_movedown.Location = new System.Drawing.Point(269, 228);
+            this.button_movedown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_movedown.Location = new System.Drawing.Point(269, 240);
             this.button_movedown.Name = "button_movedown";
             this.button_movedown.Size = new System.Drawing.Size(81, 23);
             this.button_movedown.TabIndex = 6;
@@ -113,8 +136,9 @@
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 263);
+            this.label1.Location = new System.Drawing.Point(12, 275);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(104, 15);
             this.label1.TabIndex = 7;
@@ -122,8 +146,9 @@
             // 
             // label2
             // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(194, 263);
+            this.label2.Location = new System.Drawing.Point(194, 275);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(106, 15);
             this.label2.TabIndex = 8;
@@ -131,19 +156,21 @@
             // 
             // comboBox_bgcolor
             // 
+            this.comboBox_bgcolor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.comboBox_bgcolor.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.comboBox_bgcolor.FormattingEnabled = true;
-            this.comboBox_bgcolor.Location = new System.Drawing.Point(194, 282);
+            this.comboBox_bgcolor.Location = new System.Drawing.Point(194, 294);
             this.comboBox_bgcolor.Name = "comboBox_bgcolor";
             this.comboBox_bgcolor.Size = new System.Drawing.Size(123, 23);
             this.comboBox_bgcolor.TabIndex = 10;
             // 
             // button_fgcolor
             // 
+            this.button_fgcolor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.button_fgcolor.FlatAppearance.BorderSize = 0;
             this.button_fgcolor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_fgcolor.Image = ((System.Drawing.Image)(resources.GetObject("button_fgcolor.Image")));
-            this.button_fgcolor.Location = new System.Drawing.Point(141, 282);
+            this.button_fgcolor.Location = new System.Drawing.Point(141, 294);
             this.button_fgcolor.Name = "button_fgcolor";
             this.button_fgcolor.Size = new System.Drawing.Size(27, 23);
             this.button_fgcolor.TabIndex = 14;
@@ -152,17 +179,19 @@
             // 
             // comboBox_fgcolor
             // 
+            this.comboBox_fgcolor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.comboBox_fgcolor.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.comboBox_fgcolor.FormattingEnabled = true;
-            this.comboBox_fgcolor.Location = new System.Drawing.Point(12, 282);
+            this.comboBox_fgcolor.Location = new System.Drawing.Point(12, 294);
             this.comboBox_fgcolor.Name = "comboBox_fgcolor";
             this.comboBox_fgcolor.Size = new System.Drawing.Size(123, 23);
             this.comboBox_fgcolor.TabIndex = 13;
             // 
             // label3
             // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 323);
+            this.label3.Location = new System.Drawing.Point(12, 335);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(41, 15);
             this.label3.TabIndex = 15;
@@ -170,15 +199,17 @@
             // 
             // textBox_string
             // 
-            this.textBox_string.Location = new System.Drawing.Point(12, 341);
+            this.textBox_string.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox_string.Location = new System.Drawing.Point(12, 353);
             this.textBox_string.Name = "textBox_string";
             this.textBox_string.Size = new System.Drawing.Size(338, 23);
             this.textBox_string.TabIndex = 16;
             // 
             // checkBox_ignorecase
             // 
+            this.checkBox_ignorecase.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.checkBox_ignorecase.AutoSize = true;
-            this.checkBox_ignorecase.Location = new System.Drawing.Point(12, 379);
+            this.checkBox_ignorecase.Location = new System.Drawing.Point(12, 391);
             this.checkBox_ignorecase.Name = "checkBox_ignorecase";
             this.checkBox_ignorecase.Size = new System.Drawing.Size(88, 19);
             this.checkBox_ignorecase.TabIndex = 17;
@@ -187,8 +218,9 @@
             // 
             // checkBox_bold
             // 
+            this.checkBox_bold.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.checkBox_bold.AutoSize = true;
-            this.checkBox_bold.Location = new System.Drawing.Point(106, 379);
+            this.checkBox_bold.Location = new System.Drawing.Point(106, 391);
             this.checkBox_bold.Name = "checkBox_bold";
             this.checkBox_bold.Size = new System.Drawing.Size(50, 19);
             this.checkBox_bold.TabIndex = 18;
@@ -197,10 +229,11 @@
             // 
             // button_bgcolor
             // 
+            this.button_bgcolor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.button_bgcolor.FlatAppearance.BorderSize = 0;
             this.button_bgcolor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_bgcolor.Image = ((System.Drawing.Image)(resources.GetObject("button_bgcolor.Image")));
-            this.button_bgcolor.Location = new System.Drawing.Point(323, 282);
+            this.button_bgcolor.Location = new System.Drawing.Point(323, 294);
             this.button_bgcolor.Name = "button_bgcolor";
             this.button_bgcolor.Size = new System.Drawing.Size(27, 23);
             this.button_bgcolor.TabIndex = 19;
@@ -211,7 +244,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(362, 449);
+            this.ClientSize = new System.Drawing.Size(362, 461);
             this.Controls.Add(this.button_bgcolor);
             this.Controls.Add(this.checkBox_bold);
             this.Controls.Add(this.checkBox_ignorecase);
@@ -230,6 +263,8 @@
             this.Controls.Add(this.button_cancel);
             this.Controls.Add(this.button_ok);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(378, 800);
+            this.MinimumSize = new System.Drawing.Size(378, 500);
             this.Name = "Form2_Highlight";
             this.Text = "Highlight";
             this.ResumeLayout(false);
@@ -256,5 +291,6 @@
         private CheckBox checkBox_ignorecase;
         private CheckBox checkBox_bold;
         private Button button_bgcolor;
+        private ColumnHeader columnHeader1;
     }
 }
--- Form2_Highlight.cs
+++ Form2_Highlight.cs
@@ -13,21 +13,69 @@ namespace serialog
     public partial class Form2_Highlight : Form
     {
         static public HighlightSettings highlightSettings = new HighlightSettings();
+        private HighlightSettings tempHighlightSettings = new HighlightSettings();
+
+        private void AddHighlightEntryToListView(ref ListView listView, HighlightEntry highlightEntry)
+        {
+            listView.Items.Add(highlightEntry.text);
+            listView.Items[listView1.Items.Count - 1].ForeColor = highlightEntry.foreColor;
+            listView.Items[listView1.Items.Count - 1].BackColor = highlightEntry.backColor;
+        }
 
         public Form2_Highlight()
         {
             InitializeComponent();
+
+            // copy to temp
+            tempHighlightSettings = highlightSettings;
+
+            foreach(var item in tempHighlightSettings.highlightEntries)
+            {
+                AddHighlightEntryToListView(ref listView1, item);
+            }            
         }
 
         static public void InitializeHighlightSettings()
         {
+            // Init highlightSettings from a file saved somewhere
+
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button_add_Click(object sender, EventArgs e)
         {
-            
+            Color bgcol;
+            if (comboBox_bgcolor.Text.Length == 0)
+                bgcol = Color.White;
+            else
+                bgcol = Color.FromName(comboBox_bgcolor.Text);
+
+            HighlightEntry highlightEntry = new HighlightEntry(textBox_string.Text, 
+                Color.FromName(comboBox_fgcolor.Text), bgcol, 
+                checkBox_bold.Checked, checkBox_ignorecase.Checked);
+
+            tempHighlightSettings.Add(highlightEntry);
+            AddHighlightEntryToListView(ref listView1, highlightEntry);
+        }
+
+        private void button_cancel_Click(object sender, EventArgs e)
+        {
             this.Close();
+        }
+
+        private void button_ok_Click(object sender, EventArgs e)
+        {
+            highlightSettings = tempHighlightSettings;
+            this.Close();
+        }
+
+        private void button_delete_Click(object sender, EventArgs e)
+        {
+            foreach(int index in listView1.SelectedIndices)
+            {
+                tempHighlightSettings.RemoveAt(index);
+                listView1.Items.RemoveAt(index);
+            }
         }
     }
 
@@ -35,8 +83,8 @@ namespace serialog
     public class HighlightEntry
     {
         public string text;
-        public Color foreColor = Color.White;
-        public Color backColor = Color.Black;
+        public Color foreColor = Color.Black;
+        public Color backColor = Color.White;
         public bool bold = false;
         public bool italic = false;
         public bool caseSensitive = true;
@@ -48,7 +96,7 @@ namespace serialog
         public HighlightEntry(string text, Color foreColor)
         {
             this.text = text;
-            this.foreColor = Color.White;
+            this.foreColor = foreColor;
         }
         public HighlightEntry(string text, Color foreColor, Color backColor)
         {
@@ -75,9 +123,9 @@ namespace serialog
             highlightEntries.Add(highlightEntry);
         }
 
-        public void Remove(HighlightEntry highlightEntry)
+        public void RemoveAt(int idx)
         {
-            highlightEntries.Remove(highlightEntry);
+            highlightEntries.RemoveAt(idx);
         }
 
         public void Clear()
