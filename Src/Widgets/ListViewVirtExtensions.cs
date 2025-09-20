0776b7767354ecb6346762a561b289e90e0af972
erik.juvan <erik.juvan@gorenje.com>
2025-09-20 22:12:51 +0200

    Implement find highlights

--- Src/Widgets/ListViewVirtExtensions.cs
+++ Src/Widgets/ListViewVirtExtensions.cs
@@ -92,5 +92,56 @@
             }
             return false;
         }
+
+        // ///////// //
+        // Highlight //
+        // ///////// //
+
+        public static bool FindNextHighlighted(this ListViewVirt listView)
+        {
+            if (listView.DataLog.Count == 0)
+                return false;
+
+            int startIndex = listView.SelectedIndices.Count > 0
+                ? listView.SelectedIndices[^1] + 1
+                : 0;
+
+            return listView.SelectHighlighted(startIndex, forward: true);
+        }
+
+        public static bool FindPrevHighlighted(this ListViewVirt listView)
+        {
+            if (listView.DataLog.Count == 0)
+                return false;
+
+            int startIndex = listView.SelectedIndices.Count > 0
+                ? listView.SelectedIndices[0] - 1
+                : listView.DataLog.Count - 1;
+
+            return listView.SelectHighlighted(startIndex, forward: false);
+        }
+
+        public static bool FindFirstHighlighted(this ListViewVirt listView) =>
+            listView.SelectHighlighted(0, forward: true);
+
+        public static bool FindLastHighlighted(this ListViewVirt listView) =>
+            listView.SelectHighlighted(listView.DataLog.Count - 1, forward: false);
+
+        private static bool SelectHighlighted(this ListViewVirt listView, int startIndex, bool forward)
+        {
+            int i = startIndex;
+            while (i >= 0 && i < listView.DataLog.Count)
+            {
+                if (listView.FindHighlightMatch(listView.DataLog[i].GetContent()) != null)
+                {
+                    listView.SelectedIndices.Clear();
+                    listView.SelectedIndices.Add(i);
+                    listView.EnsureVisible(i);
+                    return true;
+                }
+                i += forward ? 1 : -1;
+            }
+            return false;
+        }
     }
 }
